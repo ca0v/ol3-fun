a8f7123643c00a698651fb1bd585b3cc7e4f0462
calix <Corey.Alix@infor.com>
2018-08-24 10:36:15 -0400

    switch from expect.js to expect

--- test/asarray.test.ts
+++ test/asarray.test.ts
@@ -1,9 +1,12 @@
 // already loaded as globals...not sure how, requires declaration for tsc to compile...what's going on here?
-declare var describe: any;
-declare var it: any;
+// declare var describe: any;
+// declare var it: any;
 declare var require: any;
 
-let expect = require("expect.js");
+// unsure how to do this using standard syntax and typings don't seem to correlate with the "expect" I'm getting
+// // Type definitions for Expect 1.20, expect.version = '0.3.1' so I might be loading the wrong defs!
+//import expect from "expect";
+var expect = require("expect");
 
 import { asArray, uuid } from "../ol3-fun/common";
 
@@ -14,6 +17,7 @@ function should(result: boolean, msg: string) {
 describe("expect", () => {
     it("expect", () => {
         should(!!expect, "expect is defined");
+        expect(true).toBe(true);
     });
 });
 
