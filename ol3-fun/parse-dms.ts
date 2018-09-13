43ede2d5ad37f82918f52464a53fa38a72c3993f
coreyalix <coreyalix@hotmail.com>
2018-09-13 06:53:15 -0400

    adds lonlat -> dms

--- ol3-fun/parse-dms.ts
+++ ol3-fun/parse-dms.ts
@@ -1,97 +1,127 @@
-// ported from https://github.com/gmaclennan/parse-dms/blob/master/index.js
+/**
+ * Converts DMS to lonlat
+ * ported from https://github.com/gmaclennan/parse-dms/blob/master/index.js
+ * and https://stackoverflow.com/questions/37893131/how-to-convert-lat-long-from-decimal-degrees-to-dms-format
+ */
 
 type Direction = "-" | "N" | "S" | "E" | "W";
 
 function decDegFromMatch(m: Direction[]) {
-    var signIndex = {
-        "-": -1,
-        "N": 1,
-        "S": -1,
-        "E": 1,
-        "W": -1
-    };
+	var signIndex = {
+		"-": -1,
+		N: 1,
+		S: -1,
+		E: 1,
+		W: -1
+	};
 
-    var latLonIndex = {
-        "-": "",
-        "N": "lat",
-        "S": "lat",
-        "E": "lon",
-        "W": "lon"
-    };
-    var degrees, minutes, seconds, sign, latLon;
+	var latLonIndex = {
+		"-": "",
+		N: "lat",
+		S: "lat",
+		E: "lon",
+		W: "lon"
+	};
+	var degrees, minutes, seconds, sign, latLon;
 
-    sign = signIndex[m[2]] || signIndex[m[1]] || signIndex[m[6]] || 1;
-    degrees = Number(m[3]);
-    minutes = m[4] ? Number(m[4]) : 0;
-    seconds = m[5] ? Number(m[5]) : 0;
-    latLon = latLonIndex[m[1]] || latLonIndex[m[6]];
+	sign = signIndex[m[2]] || signIndex[m[1]] || signIndex[m[6]] || 1;
+	degrees = Number(m[3]);
+	minutes = m[4] ? Number(m[4]) : 0;
+	seconds = m[5] ? Number(m[5]) : 0;
+	latLon = latLonIndex[m[1]] || latLonIndex[m[6]];
 
-    if (!inRange(degrees, 0, 180)) throw 'Degrees out of range';
-    if (!inRange(minutes, 0, 60)) throw 'Minutes out of range';
-    if (!inRange(seconds, 0, 60)) throw 'Seconds out of range';
+	if (!inRange(degrees, 0, 180)) throw "Degrees out of range";
+	if (!inRange(minutes, 0, 60)) throw "Minutes out of range";
+	if (!inRange(seconds, 0, 60)) throw "Seconds out of range";
 
-    return {
-        decDeg: sign * (degrees + minutes / 60 + seconds / 3600),
-        latLon: latLon
-    };
+	return {
+		decDeg: sign * (degrees + minutes / 60 + seconds / 3600),
+		latLon: latLon
+	};
 }
 
 function inRange(value: number, a: number, b: number) {
-    return value >= a && value <= b;
+	return value >= a && value <= b;
 }
 
-export function parse(dmsString: string): { lon: number; lat: number } | number {
+function toDegreesMinutesAndSeconds(coordinate: number) {
+	let absolute = Math.abs(coordinate);
+	let degrees = Math.floor(absolute);
+	let minutesNotTruncated = (absolute - degrees) * 60;
+	let minutes = Math.floor(minutesNotTruncated);
+	let seconds = Math.floor((minutesNotTruncated - minutes) * 60);
+	return `${degrees} ${minutes} ${seconds}`;
+}
 
-    dmsString = dmsString.trim();
+function fromLonLatToDms(lon: number, lat: number) {
+	let latitude = toDegreesMinutesAndSeconds(lat);
+	let latitudeCardinal = lat >= 0 ? "N" : "S";
+	let longitude = toDegreesMinutesAndSeconds(lon);
+	let longitudeCardinal = lon >= 0 ? "E" : "W";
+	return `${latitude} ${latitudeCardinal} ${longitude} ${longitudeCardinal}`;
+}
 
-    // Inspired by https://gist.github.com/JeffJacobson/2955437
-    // See https://regex101.com/r/kS2zR1/3
-    var dmsRe = /([NSEW])?(-)?(\d+(?:\.\d+)?)[°º:d\s]?\s?(?:(\d+(?:\.\d+)?)['’‘′:]\s?(?:(\d{1,2}(?:\.\d+)?)(?:"|″|’’|'')?)?)?\s?([NSEW])?/i;
+function fromDmsToLonLat(dmsString: string): { lon: number; lat: number } | number {
+	dmsString = dmsString.trim();
 
-    var dmsString2;
+	// Inspired by https://gist.github.com/JeffJacobson/2955437
+	// See https://regex101.com/r/kS2zR1/3
+	var dmsRe = /([NSEW])?(-)?(\d+(?:\.\d+)?)[°º:d\s]?\s?(?:(\d+(?:\.\d+)?)['’‘′:]\s?(?:(\d{1,2}(?:\.\d+)?)(?:"|″|’’|'')?)?)?\s?([NSEW])?/i;
 
-    let m1 = <Direction[]>dmsString.match(dmsRe);
+	var dmsString2;
 
-    if (!m1) throw 'Could not parse string';
+	let m1 = <Direction[]>dmsString.match(dmsRe);
 
-    // If dmsString starts with a hemisphere letter, then the regex can also capture the 
-    // hemisphere letter for the second coordinate pair if also in the string
-    if (m1[1]) {
-        m1[6] = undefined;
-        dmsString2 = dmsString.substr(m1[0].length - 1).trim();
-    } else {
-        dmsString2 = dmsString.substr(m1[0].length).trim();
-    }
+	if (!m1) throw "Could not parse string";
 
-    let decDeg1 = decDegFromMatch(m1);
+	// If dmsString starts with a hemisphere letter, then the regex can also capture the
+	// hemisphere letter for the second coordinate pair if also in the string
+	if (m1[1]) {
+		m1[6] = undefined;
+		dmsString2 = dmsString.substr(m1[0].length - 1).trim();
+	} else {
+		dmsString2 = dmsString.substr(m1[0].length).trim();
+	}
 
-    let m2 = <Direction[]>dmsString2.match(dmsRe);
+	let decDeg1 = decDegFromMatch(m1);
 
-    let decDeg2 = m2 && decDegFromMatch(m2);
+	let m2 = <Direction[]>dmsString2.match(dmsRe);
 
-    if (typeof decDeg1.latLon === 'undefined') {
-        if (!isNaN(decDeg1.decDeg) && decDeg2 && isNaN(decDeg2.decDeg)) {
-            // If we only have one coordinate but we have no hemisphere value,
-            // just return the decDeg number
-            return decDeg1.decDeg;
-        } else if (!isNaN(decDeg1.decDeg) && decDeg2 && !isNaN(decDeg2.decDeg)) {
-            // If no hemisphere letter but we have two coordinates,
-            // infer that the first is lat, the second lon
-            decDeg1.latLon = 'lat';
-            decDeg2.latLon = 'lon';
-        } else {
-            throw 'Could not parse string';
-        }
-    }
+	let decDeg2 = m2 && decDegFromMatch(m2);
 
-    // If we parsed the first coordinate as lat or lon, then assume the second is the other
-    if (typeof decDeg2.latLon === 'undefined') {
-        decDeg2.latLon = decDeg1.latLon === 'lat' ? 'lon' : 'lat';
-    }
+	if (typeof decDeg1.latLon === "undefined") {
+		if (!isNaN(decDeg1.decDeg) && decDeg2 && isNaN(decDeg2.decDeg)) {
+			// If we only have one coordinate but we have no hemisphere value,
+			// just return the decDeg number
+			return decDeg1.decDeg;
+		} else if (!isNaN(decDeg1.decDeg) && decDeg2 && !isNaN(decDeg2.decDeg)) {
+			// If no hemisphere letter but we have two coordinates,
+			// infer that the first is lat, the second lon
+			decDeg1.latLon = "lat";
+			decDeg2.latLon = "lon";
+		} else {
+			throw "Could not parse string";
+		}
+	}
 
-    return <{ lon: number; lat: number }>{
-        [decDeg1.latLon]: decDeg1.decDeg,
-        [decDeg2.latLon]: decDeg2.decDeg
-    };
+	// If we parsed the first coordinate as lat or lon, then assume the second is the other
+	if (typeof decDeg2.latLon === "undefined") {
+		decDeg2.latLon = decDeg1.latLon === "lat" ? "lon" : "lat";
+	}
 
+	return <{ lon: number; lat: number }>{
+		[decDeg1.latLon]: decDeg1.decDeg,
+		[decDeg2.latLon]: decDeg2.decDeg
+	};
+}
+
+/**
+ * Converts DMS<->LonLat
+ * @param value A DMS string or lonlat coordinate to be converted
+ */
+export function parse(value: { lon: number; lat: number }): string;
+export function parse(value: string): { lon: number; lat: number } | number;
+export function parse(value: string | { lon: number; lat: number }): { lon: number; lat: number } | number | string {
+	if (typeof value === "string") return fromDmsToLonLat(value);
+	return fromLonLatToDms(value.lon, value.lat);
 }
